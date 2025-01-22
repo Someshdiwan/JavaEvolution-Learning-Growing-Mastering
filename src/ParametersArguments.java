77d308221d7f51b0b68fa82442141577ea52f454
Someshdiwan <Someshdiwan369@gmail.com>
2025-01-22 23:25:01 +0530

    Functions and arguments done.

---
+++ src/ParametersArguments.java
@@ -0,0 +1,36 @@
+/*In Java, parameters are the variables listed in the method definition. They act as placeholders for the values that the method can accept. For example, in the method:
+
+public static int addNumbers(int a, int b) {
+    return a + b;
+}
+Here, a and b are parameters. They define what kind of values the method addNumbers can accept (in this case, two integers).
+
+When you call the method, you pass arguments (actual values) to these parameters. For example:
+
+addNumbers(3, 5);
+Here, 3 and 5 are arguments passed to the parameters a and b.
+
+        In short:
+
+Parameters are defined in the method signature.
+Arguments are the actual values passed to the method when it is called.
+
+        Parameters:
+
+Defined in the method signature.
+Act as placeholders for the values the method will receive.
+Example: In public static void greet(String name), name is a parameter.
+Arguments:
+
+The actual values passed to the method when it is called.
+        Example: In greet("Alice"), "Alice" is an argument passed to the name parameter.
+Key Difference:
+Parameters are part of the method definition (what the method expects).
+Arguments are the actual values you provide when calling the method.*/
+
+
+public class ParametersArguments {
+    public static void main(String[] args) {
+
+    }
+}

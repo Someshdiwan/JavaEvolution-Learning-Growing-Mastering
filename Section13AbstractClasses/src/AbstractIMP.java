5617c6569cc2dfbc7c63b999a8efbfa5ff093a46
Someshdiwan <Someshdiwan369@gmail.com>
2025-02-03 00:09:42 +0530

    Done.

---
+++ Section13AbstractClasses/src/AbstractIMP.java
@@ -0,0 +1,49 @@
+import java.awt.*;
+abstract class ShapeVIMP
+{
+    abstract public double peimeterIMP();
+    abstract public double areaIMP();
+}
+
+class CircleVIMP extends ShapeVIMP
+{
+    double radiusIMP;
+
+    public double peimeterIMP()
+    {
+        return 2 * Math.PI * radiusIMP;
+    }
+
+    public double areaIMP()
+    {
+        return Math.PI*radiusIMP*radiusIMP;
+    }
+}
+
+class Rectangle extends ShapeVIMP
+{
+    double length;
+    double breadth;
+
+    public double peimeterIMP()
+    {
+        return 2*(length+breadth);
+    }
+    public double areaIMP()
+    {
+        return length*breadth;
+    }
+}
+
+class AbstractIMP
+{
+    public static void main(String[] args)
+    {
+        Rectangle r=new Rectangle();
+        r.length=10;
+        r.breadth=5;
+
+        ShapeVIMP s=r;
+        System.out.println(s.areaIMP());
+    }
+}
---
+++ Section14Interfaces/src/Interfaces.java
@@ -0,0 +1,35 @@
+interface Test{
+    void meth1();
+    void meth2();
+}
+class My implements Test
+{
+    @Override
+
+    public void meth1()
+    {
+        System.out.println("Meth 1 of My Class");
+    }
+    public void meth2()
+    {
+        System.out.println("Meth 2 of my class");
+    }
+
+    public void mehth3()
+    {
+        System.out.println("meth 3 my class");
+    }
+}
+public class Interfaces
+{
+    public static void main(String[] args)
+    {
+        Test t = new My();
+        t.meth1();
+        t.meth2();
+        //Achieving dynamic method dispatch in short Run Time polymorphism. Method Overriding
+
+        // Uncommenting the below line will cause a compilation error
+        // t.meth3(); // Not accessible because `t` is of type Test
+    }
+}

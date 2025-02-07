7f7ef56178d8da23dd6fcca386f1abb35d31aec4
Someshdiwan <Someshdiwan369@gmail.com>
2025-02-07 23:52:58 +0530

    Great Going

---
+++ Section18ExceptionHandling/src/ExceptionDemo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int c;
+
+        try {
+            c = a / b;
+         /*
+         This is without a try catch block.
+
+         Program terminiate and not printing rest of the statements.n
+         Exception in thread "main" java.lang.ArithmeticException: / by zero
+         at ExceptionDemo.main(ExceptionDemo.java:7)
+         */
+
+            System.out.println(c);
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println("Denominator should not be Zero(0) try again: "+e);
+        }
+        System.out.println("Bye");
+    }
+}
---
+++ Section18ExceptionHandling/src/TryWithResources.java
@@ -0,0 +1,10 @@
+
+
+
+public class TryWithResources {
+
+
+    public static void main(String[] args) {
+
+    }
+}

8cb48ae56e9075bb54e9c304076f845fe04774b1
Someshdiwan <Someshdiwan369@gmail.com>
2025-02-18 23:59:13 +0530

    Grind hard today. Finish strong. Remember it forever.

---
+++ Section5OperatorExpression/src/Downcasting.java
@@ -0,0 +1,6 @@
+//Downcasting: Similarly, downcasting means the typecasting of a parent object to a child object.
+//Downcasting cannot be implicit.
+
+public class Downcasting {
+    
+}
---
+++ Section6StringClassPrinting/Engineering Digest ED/src/Substring.java
@@ -0,0 +1,7 @@
+public class Substring {
+    public static void main(String[] args) {
+        String name = "Virat Kholi";
+        String substring = name.substring(5);
+        System.out.println(substring);
+    }
+}

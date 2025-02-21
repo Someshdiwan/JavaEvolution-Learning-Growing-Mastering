1af705aad0d6bb28af97b9aa1e547c30e7641647
Someshdiwan <Someshdiwan369@gmail.com>
2025-02-21 23:50:38 +0530

    Grind hard today. Finish strong. Remember it forever.

---
+++ Section11ObjectOrientedProgramming/OOPs 2.O/src/Test/Animal.java
@@ -0,0 +1,12 @@
+package Test;
+public class Animal
+{
+    String name;
+    String age;
+
+    //Method Behavior hota hai
+    public void makeSound()
+    {
+        System.out.println("MAke Some sound");
+    }
+}
---
+++ Section14Interfaces/src/Animal.java
@@ -0,0 +1,49 @@
+//Abstract Method
+//Static Constant
+
+interface Animal
+{
+        public static int MAX_AGE = 100;
+
+        public abstract void eat();
+        void sleep();
+
+
+        //static methods
+        public static void info()
+        {
+            System.out.println("THis is an animal interface");
+        }
+}
+
+class Dog implements Animal
+{
+    public void eat(){
+        System.out.println("Dog is eating");
+    }
+    public void sleep(){
+        System.out.println("nothing just sleeping");
+    }
+}
+
+class Cat implements Animal{
+    public void eat(){
+        System.out.println("Cat is eating");
+    }
+    public void sleep(){
+        System.out.println("just sleeping");
+    }
+}
+
+class Tetst{
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+
+        Cat cat = new Cat();
+        cat.eat();
+
+        System.out.println((Dog.MAX_AGE)); //You can access from Animal also.
+        System.out.println(Animal.MAX_AGE);
+    }
+}

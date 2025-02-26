89dfce9f18bc0eb3ac2457b5bd04ace21a633806
Somesh diwan <someshdiwan369@gmail.com>
2025-02-26 23:55:04 +0530

    Grind hard.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

---
+++ Section19MultiThreading/DeadLock/src/DeadLock.java
@@ -0,0 +1,67 @@
+class Pen {
+    public synchronized void writeWithPenAndPaper(NoteNook noteNook) {
+        System.out.println(Thread.currentThread().getName() + " is using pen");
+        noteNook.finishWritinig();
+    }
+
+    public synchronized void finishWritinig() {
+        System.out.println(Thread.currentThread().getName() + " finished using " + this);
+    }
+}
+
+class NoteNook {
+    public synchronized void writeWithPaperAndPen(Pen pen) {
+        System.out.println(Thread.currentThread().getName() + "is using Paper " + this + " and trying to use pen" + this);
+        pen.finishWritinig();
+    }
+
+    public synchronized void finishWritinig() {
+        System.out.println(Thread.currentThread().getName() + " finished using paper " + this);
+    }
+}
+
+class Task1 implements Runnable {
+    private Pen pen;
+    private NoteNook noteNook;
+
+    public Task1(Pen pen, NoteNook noteNook) {
+        this.pen = pen;
+        this.noteNook = noteNook;
+    }
+
+    public void run() {
+        pen.writeWithPenAndPaper(noteNook); //Thread1 locks pen and tries to lock notebook.
+    }
+}
+
+class Task2 implements Runnable {
+    private Pen pen;
+    private NoteNook notebook;
+
+    public Task2(Pen pen, NoteNook noteNook) {
+        this.pen = pen;
+        this.notebook = noteNook;
+    }
+
+    public void run() {
+        //Preventing deadlocks
+        synchronized (pen){
+            notebook.writeWithPaperAndPen(pen); //Thread1 locks paper and tries to lock pen.
+        }
+//        notebook.writeWithPaperAndPen(pen); //Thread1 locks paper and tries to lock pen.
+    }
+}
+
+public class DeadLock {
+    public static void main(String[] args) {
+        Pen pen = new Pen();
+        NoteNook notebook = new NoteNook();
+
+        //Via constructor we are sending objects check above code
+        Thread thread1 = new Thread(new Task1(pen, notebook), "Thread One is running");
+        Thread thread2 = new Thread(new Task2(pen, notebook), "Threead Two is  running");
+
+        thread1.start();
+        thread2.start();
+    }
+}
---
+++ Section19MultiThreading/ThreadPooling/src/CyclicBarrierExample.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.*;
+
+public class CyclicBarrierExample {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        int numberOfServices = 3;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfServices);
+
+        CyclicBarrier barrier = new CyclicBarrier(numberOfServices,
+                () -> System.out.println("All services are up. Proceeding with main execution."));
+
+        executorService.submit(new DependentService(barrier));
+        executorService.submit(new DependentService(barrier));
+        executorService.submit(new DependentService(barrier));
+
+        System.out.println("Main");
+        barrier.getParties();
+        //executorService.shutdown();
+    }
+
+    static class DependentService implements Callable<String> {
+        private CyclicBarrier barrier;  // Corrected type
+
+        // Constructor now correctly accepts CyclicBarrier
+        public DependentService(CyclicBarrier barrier) {
+            this.barrier = barrier;
+        }
+
+        @Override
+        public String call() throws Exception {
+            System.out.println(Thread.currentThread().getName() + " service started.");
+            Thread.sleep(6000);
+            System.out.println(Thread.currentThread().getName() + " service completed.");
+            barrier.await();  // Wait at the barrier
+            return "Ok";
+        }
+    }
+}

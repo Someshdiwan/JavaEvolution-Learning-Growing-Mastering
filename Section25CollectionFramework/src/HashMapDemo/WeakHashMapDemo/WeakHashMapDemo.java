da7b27de4a26228f5d31721b516f32ad21ab181b
Someshdiwan <Someshdiwan369@gmail.com>
2025-03-17 16:59:25 +0530

    Revise Done.
    at 04:19 PM,
    17 March 2025.
    
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

---
+++ Section25CollectionFramework/src/HashMapDemo/WeakHashMapDemo/WeakHashMapDemo.java
@@ -0,0 +1,34 @@
+package HashMapDemo.WeakHashMapDemo;
+
+import java.util.WeakHashMap;
+
+public class WeakHashMapDemo {
+
+    public static void main(String[] args) {
+        WeakHashMap<String, Image> imageChache = new WeakHashMap<>();
+
+        imageChache.put("img1", new Image("Image 1"));
+        imageChache.put("img2",new Image("Image 2"));
+        System.out.println(imageChache);
+
+        simlateApplicationRunning();
+        System.gc();
+        System.out.println("Cache after running (some entries may be cleared): " + imageChache);
+    }
+    private static void simlateApplicationRunning(){
+        try{
+            System.out.println("Simukleting application running...");
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class Image{
+    private String name;
+    public Image(String name){
+        this.name = name;
+    }
+}

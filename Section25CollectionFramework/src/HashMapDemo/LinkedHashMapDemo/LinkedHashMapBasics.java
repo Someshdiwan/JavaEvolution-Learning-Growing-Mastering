7d905e9f3198b83a0491c6db2b00cbf4c961bc32
Someshdiwan <Someshdiwan369@gmail.com>
2025-03-17 12:26:22 +0530

    Revise Done. LinkedHashMapBasics Checking most recently access key. at 12:26 PM, 17 March 2025.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

--- Section25CollectionFramework/src/HashMapDemo/LinkedHashMapDemo/LinkedHashMapBasics.java
+++ Section25CollectionFramework/src/HashMapDemo/LinkedHashMapDemo/LinkedHashMapBasics.java
@@ -1,10 +1,11 @@
-package LinkedHashMapDemo;
+package HashMapDemo.LinkedHashMapDemo;
 
 import java.util.LinkedHashMap;
 
 public class LinkedHashMapBasics {
     public static void main(String[] args) {
         /*LinkedHashMap<Integer, String> LHM = new LinkedHashMap<>(5);*/
+
         LinkedHashMap<Integer, String> LHM = new LinkedHashMap<>(5,.75F,true);
         //true = means order of their access . 1st key show most recently used key.
         //You can change the loading factor. 0.75...
@@ -19,15 +20,15 @@ public class LinkedHashMapBasics {
 
         //Take String references for value.
         String s = LHM.get(2);
-        s = LHM.get(5);
-        s= LHM.get(1);
+        String s1 = LHM.get(5);
+        String s2= LHM.get(1);
         //Keys ko access kar Rahe hai and Most recently access key and Most recently accessed key.
         //Most recently accessed at the last and least recently accessed at the start.
 
         LHM.forEach((k,v)->System.out.println(k+" "+v));
     }
 }
-
-
-//Suppose you want to limit the size of the Linked-Hash Map then you need to override the method inside Class and anonymous class
-//must be...protected boolean ....
+/*
+Suppose you want to limit the size of the Linked-Hash Map then you need to override the method inside Class
+and anonymous class
+must be...protected boolean ....*/

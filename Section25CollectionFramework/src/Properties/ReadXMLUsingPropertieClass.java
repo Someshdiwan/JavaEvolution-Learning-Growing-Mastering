5b27e640fa77576b4eb1e7ecc6825fa8a0dfc861
Someshdiwan <Someshdiwan369@gmail.com>
2025-03-18 16:08:44 +0530

    The Properties class represents a persistent set of properties. The Properties can be saved to a stream or loaded from a stream. Each key and its corresponding value in the property list is a string.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

--- Section25CollectionFramework/src/Properties/ReadXMLUsingPropertieClass.java
+++ Section25CollectionFramework/src/Properties/ReadXMLUsingPropertieClass.java
@@ -7,11 +7,9 @@ public class ReadXMLUsingPropertieClass {
     public static void main(String[] args)throws Exception
     {
         Properties p = new Properties();
-
         p.loadFromXML(new FileInputStream("C://Users//somes//Downloads//JAVA SE//Section25CollectionFramework//src//Properties//Person.xml"));
 
         System.out.println(p);
-
         System.out.println(p.getProperty("Name: "));
     }
 }

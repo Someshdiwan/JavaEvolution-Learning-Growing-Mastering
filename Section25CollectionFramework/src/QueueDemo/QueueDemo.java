059456210dde59fa7dc81b984424aa43e77cd3da
Someshdiwan <Someshdiwan369@gmail.com>
2025-03-18 17:06:43 +0530

    A collection designed for holding elements prior to processing. Besides basic Collection operations, queues provide additional insertion, extraction, and inspection operations. Each of these methods exists in two forms: one throws an exception if the operation fails, the other returns a special value (either null or false, depending on the operation). The latter form of the insert operation is designed specifically for use with capacity-restricted Queue implementations; in most implementations, insert operations cannot fail.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

---
+++ Section25CollectionFramework/src/QueueDemo/QueueDemo.java
@@ -0,0 +1,26 @@
+package QueueDemo;
+
+import java.util.LinkedList;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        System.out.println(list);
+        System.out.println("Removed First: "+list.removeFirst());
+
+        LinkedList<Integer> list2 = new LinkedList<>();
+
+        list2.addLast(1); //Enqueue is the operation of adding an element to the end (rear) of a queue.
+        list2.addLast(2); //Enqueue
+        list2.addLast(3); //Enqueue
+
+        System.out.println(list2);
+        Integer i = list2.removeFirst(); //dequeue bolte operation.
+        System.out.println("Enqueue: "+list2); //linkedList is acting as a Queue.
+
+        System.out.println(list2.peek());
+    }
+}

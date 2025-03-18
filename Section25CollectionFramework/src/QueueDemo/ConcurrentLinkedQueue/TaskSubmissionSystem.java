150ab41c18c8c5e0209a5f4a3dc5f6c03d9b265d
Someshdiwan <Someshdiwan369@gmail.com>
2025-03-18 16:38:26 +0530

    An unbounded thread-safe queue based on linked nodes. This queue orders elements FIFO (first-in-first-out). The head of the queue is that element that has been on the queue the longest time. The tail of the queue is that element that has been on the queue the shortest time. New elements are inserted at the tail of the queue, and the queue retrieval operations obtain elements at the head of the queue. A ConcurrentLinkedQueue is an appropriate choice when many threads will share access to a common collection. Like most other concurrent collection implementations, this class does not permit the use of null elements.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

---
+++ Section25CollectionFramework/src/QueueDemo/ConcurrentLinkedQueue/TaskSubmissionSystem.java
@@ -0,0 +1,42 @@
+package QueueDemo.ConcurrentLinkedQueue;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * MAP --> THREAD SAFE --> ConcurrentHashMap
+ *
+ * @see ConcurrentMap.ConcurrentHashMapDemo.ConcurrentHashMapDemo
+ */
+
+public class TaskSubmissionSystem {
+    private static ConcurrentLinkedQueue<String> taskQueue = new ConcurrentLinkedQueue<>();
+
+    public static void main(String[] args) {
+
+        Thread producer = new Thread(() -> {
+            while (true) {
+                try {
+                    taskQueue.add("Task " + System.currentTimeMillis());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread consumer = new Thread(() -> {
+            while (true) {
+                try {
+                    String task = taskQueue.poll();
+                    System.out.println("Processing: " + task);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        producer.start();
+        consumer.start();
+    }
+}

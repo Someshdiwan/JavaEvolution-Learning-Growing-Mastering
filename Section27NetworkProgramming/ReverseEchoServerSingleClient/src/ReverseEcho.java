3da6f6ec47d85a32fa91713876bbce7c1367c23c
Someshdiwan <Someshdiwan369@gmail.com>
2025-03-21 19:45:12 +0530

    feat: Implement basic client-server communication using Java sockets
    - Created `ReverseEcho` server:
      - Listens on port `2000` for incoming client connections.
      - Reads messages sent by the client.
      - Reverses the received message and sends it back.
      - Terminates when the client sends "dne".
    
    - Created `Client` class:
      - Connects to the server using the specified IP and port `2000`.
      - Reads input from the user and sends it to the server.
      - Receives reversed messages from the server and displays them.
      - Ends communication when "dne" is received.
    
    - Improvements:
      - Used `BufferedReader` for efficient text input/output handling.
      - Utilized `StringBuilder` for string reversal.
      - Ensured proper socket communication with PrintStream.
    
    Next Steps:
    - Implement multi-threading to allow multiple clients.
    - Improve error handling for better stability.
    
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

---
+++ Section27NetworkProgramming/ReverseEchoServerSingleClient/src/ReverseEcho.java
@@ -0,0 +1,61 @@
+import java.net.*;
+import java.io.*;
+
+public class ReverseEcho {
+    public static void main(String[] args) throws Exception
+    {
+        ServerSocket ss = new ServerSocket(2000);
+        Socket stk = ss.accept();
+
+        //Reading complete line from client.
+        BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        //Print output to the client.
+        PrintStream ps = new PrintStream(stk.getOutputStream());
+
+        String msg;
+        //Reading a msg from a client and reverse it back and send to client.
+        StringBuilder sb;
+        //References deceleration.
+        do
+        {
+            msg = br.readLine();
+            //reversing a msg from client using method present inside the StringBuilder.
+            /*StringBuilder sb = new StringBuilder(msg);*/
+            sb=new StringBuilder(msg);
+            sb.reverse();
+            msg=sb.toString();
+
+            //write back to the output stream of socket.
+            ps.println(msg);
+        }while(!msg.equals("dne"));
+        //Server part is ready.
+    }
+}
+
+class Client
+{
+    public static void main(String[] args)throws Exception
+    {
+        Socket stk = new Socket("Add Your Device IP Address",2000);
+
+        //Read from the keyboard.
+        BufferedReader keyb = new BufferedReader(new InputStreamReader(System.in));
+
+        //Reading from socket whatever it, coming from the server.
+        //also you have to print it what you have read it.
+        BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        PrintStream ps = new PrintStream(stk.getOutputStream());
+
+        String msg;
+        //StringBuilder sb; no need here because we are not reading it from client side.
+        do
+        {
+            msg= keyb.readLine();
+            //After reading from the keyboard it send it to the server.
+
+            ps.println(msg);
+            msg=br.readLine();
+            System.out.println("From Server: "+msg);
+        }while (!msg.equals("dne"));
+    }
+}

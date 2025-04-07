b1a4c9631e400628693ebd5519a4f6b881059a93
Someshdiwan <Someshdiwan369@gmail.com>
2025-04-08 02:14:53 +0530

    Added Offline reading support page.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

--- site/assets/sw.js
+++ site/assets/sw.js
@@ -1,7 +1,30 @@
+const cacheName = 'java-evolution-cache-v1';
+const assetsToCache = [
+  '/',
+  '/JavaEvolution-Learning-Growing-Mastering/index.html',
+  '/JavaEvolution-Learning-Growing-Mastering/assets/style.css',
+  '/JavaEvolution-Learning-Growing-Mastering/assets/script.js',
+  // Add more files as needed
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
+
+/*
 self.addEventListener("install", function (e) {
   console.log("Service Worker Installed");
 });
 
 self.addEventListener("fetch", function (e) {
   // Just let requests pass through for now
-});
+});*/

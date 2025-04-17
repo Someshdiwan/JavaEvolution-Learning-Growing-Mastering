cc114bd8084224f423081b5d6d0a66d9bc1a3c61
Someshdiwan <Someshdiwan369@gmail.com>
2025-04-17 23:47:56 +0530

    Added Practice Program.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

---
+++ Section7ConditionalStatements/src/SwitchCodeCC2.java
@@ -0,0 +1,41 @@
+/*
+Arithmetic operation:
+
+Write a C++ program that takes two numbers and an operator (+, -, *, /) as input.
+
+Use a 'switch' statement to perform the corresponding arithmetic operation and print the result.
+
+Check the sample input / output below for clarity.
+
+(Note: It is guaranteed that in the hidden test cases, the second number will not be 0 during division.)
+*/
+
+import java.util.Scanner;
+
+public class SwitchCodeCC2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        //Take two integer as a input and choose a one operator's below this.
+        char op = scanner.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println(num1 + num2);
+                break;
+            case '-':
+                System.out.println(num1 - num2);
+                break;
+            case '*':
+                System.out.println(num1 * num2);
+                break;
+            case '/':
+                System.out.println(num1 / num2);
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}

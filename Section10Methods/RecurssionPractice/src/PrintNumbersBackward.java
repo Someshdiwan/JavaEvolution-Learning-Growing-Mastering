f91b28f7fa27c0f0d860452a64e84a7fbd0b85a4
Somesh diwan <someshdiwan369@gmail.com>
2025-05-03 19:35:11 +0530

    Practice Done.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

---
+++ Section10Methods/RecurssionPractice/src/PrintNumbersBackward.java
@@ -0,0 +1,16 @@
+public class PrintNumbersBackward {
+    public static void main(String[] args) {
+        countBackwards(7);
+    }
+    public static void countBackwards(int n){
+        if(n==0)
+        {
+            System.out.println("Done!");
+        }
+        else{
+            System.out.println(n);
+            n--;
+            countBackwards(n);
+        }
+    }
+}

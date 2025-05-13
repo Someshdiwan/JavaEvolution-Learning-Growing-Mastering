6a8b7413d13540a4e59dcdb6e6342f6841c38f2c
Someshdiwan <Someshdiwan369@gmail.com>
2025-05-13 23:44:37 +0530

    Added Tips and Tricks.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

---
+++ Section18ExceptionHandling/src/CustomExceptions.java
@@ -0,0 +1,35 @@
+// Submit to check the output
+import java.util.*;
+
+class Rectangle {
+    public static int calculateArea(int length, int width) throws NegativeValueException {
+        if (length < 0 || width < 0) {
+            throw new NegativeValueException("Error: length or width cannot be negative");
+        }
+        return length * width;
+    }
+}
+
+class NegativeValueException extends Exception {
+    public NegativeValueException(String message) {
+        super(message);
+    }
+}
+
+class CustomExceptions
+{
+    public static void main (String[] args)
+    {
+        try {
+            int length = -5;
+            int width = 3;
+
+            Rectangle rectangle = new Rectangle();
+            int area = rectangle.calculateArea(length, width);
+            System.out.println("Area: " + area);
+        }
+        catch (NegativeValueException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}

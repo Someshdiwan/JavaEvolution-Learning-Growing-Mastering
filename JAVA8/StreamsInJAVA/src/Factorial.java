61048480da7aa4ccaba0da1139aef765168ea0fa
Someshdiwan <Someshdiwan369@gmail.com>
2025-05-23 23:47:05 +0530

    Added Practice Code.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

---
+++ JAVA8/StreamsInJAVA/src/Factorial.java
@@ -0,0 +1,18 @@
+import java.util.stream.IntStream;
+
+class Factorial {
+
+    static void printFactorial(int n) {
+        long factorial = IntStream.rangeClosed(1, n)
+                .reduce(1, (int a, int b) -> a * b);
+        System.out.println("The factorial of the given number is: " + factorial);
+    }
+
+    static void solve(int n) {
+        printFactorial(n);
+    }
+
+    public static void main(String[] args) {
+        solve(6);
+    }
+}

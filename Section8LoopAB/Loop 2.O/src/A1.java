aba563fb55654c1fd56640eea79aea50a001a624
Someshdiwan <Someshdiwan369@gmail.com>
2025-06-06 23:56:05 +0530

    For loop practice.
    Signed-off-by: Someshdiwan <Someshdiwan369@gmail.com>

---
+++ Section8LoopAB/Loop 2.O/src/A1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class A1
+{
+    public static void main(String[] args)
+    {
+        int [] A = {11,22,33,44,55};
+        
+        for(int i=0; i<A.length; i++)
+        {
+            System.out.println(A[i]);
+        }
+    }
+}

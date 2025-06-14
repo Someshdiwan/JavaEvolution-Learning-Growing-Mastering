d269a60d699460163d3391b32340f14d42c3001e
Somesh diwan <someshdiwan369@gmail.com>
2025-06-14 19:33:47 +0530

    feat: Add Ticket class to calculate total ticket price with GST and discount
    Add a program that:
    - Calculates total price for adult and child tickets
    - Applies 18% GST to the total ticket price
    - Applies a 10% discount on the price after GST
    - Outputs the final payable amount
    - Demonstrates arithmetic operations and order of operations in Java.

--- Section5OperatorExpression/src/Ticket.java
+++ Section5OperatorExpression/src/Ticket.java
@@ -1,10 +1,16 @@
 class Ticket {
     public static void main(String[] args) {
-        int a = 5, c = 3, at = 1000, ct = 500;
+        int a = 5, c = 3,
+                at = 1000, ct = 500;
+
         int tp = (a*at) + (c*ct);
+
         double gst = 0.18 * tp;
+
         double pbd = tp+gst;
+
         double dp = 0.1 * pbd;
+
         double fp = pbd - dp;
 
         System.out.println("Final Price is: "+fp);

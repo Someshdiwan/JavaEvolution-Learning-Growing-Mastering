270c1f76713a7ad355588a8368bf7c2be74abe4b
Somesh diwan <someshdiwan369@gmail.com>
2025-06-14 19:31:46 +0530

    feat: Add ReadingFromKeyBoard class to read integers and compute their sum
    Add a program that:
    - Uses `Scanner` to read two integers from user input
    - Computes and prints their sum
    - Demonstrates basic input handling and arithmetic operations in Java.

--- Section5OperatorExpression/src/ReadingFromKeyBoard.java
+++ Section5OperatorExpression/src/ReadingFromKeyBoard.java
@@ -2,14 +2,15 @@ import java.lang.*;
 import java.util.*;
 
 public class ReadingFromKeyBoard {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         int a, b, c;
-        System.out.print("Enter 2 number");
+        System.out.print("Enter 2 number: ");
+
         a = s.nextInt();
         b = s.nextInt();
         c = a+b;
-        System.out.println("Sum of two number is"+ c);
+
+        System.out.println("Sum of two number is: "+ c);
     }
 }

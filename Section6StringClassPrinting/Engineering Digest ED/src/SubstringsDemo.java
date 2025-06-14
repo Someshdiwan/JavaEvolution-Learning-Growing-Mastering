28db03f4f2857867e122bdaf282107eabee8ac19
Somesh diwan <someshdiwan369@gmail.com>
2025-06-14 21:24:54 +0530

    feat: Add SubstringsDemo class to demonstrate Java substring(beginIndex, endIndex) method
    - Created a `SubstringsDemo` class with examples using the `substring(int beginIndex, int endIndex)` method.
    - Defined two sample strings:
      - `alphabet = "ABCDEFG"` (indexes A:0 to G:6)
      - `keyboard = "QWERTY"` (indexes Q:0 to Y:5)
    - Used `alphabet.substring(1, 7)` to extract "BCDEFG", which includes characters from index 1 up to (but not including) index 7.
    - Used `keyboard.substring(0, 5)` to extract "QWERT", which includes characters from index 0 up to index 5.
    - Printed both results to demonstrate how substring slicing works in Java.
    
    This commit serves as a practical illustration of extracting portions of strings using index ranges.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section6StringClassPrinting/Engineering Digest ED/src/SubstringsDemo.java
+++ Section6StringClassPrinting/Engineering Digest ED/src/SubstringsDemo.java
@@ -1,9 +1,8 @@
-public class SubstringsDemo
-{
-    public static void main(String[] args)
-    {
+public class SubstringsDemo {
+    public static void main(String[] args) {
         String alphabet = "ABCDEFG";
         //letter: index. A:0, B:1, C:2, D:3, E:4, F:5, G:6
+
         String keyboard = "QWERTY";
         //letter: index. Q:0, W:1, E:2, R:3, T:4, Y:5
 

92b0a05d476173c9012aae0a0acd634b66db7aad
Somesh diwan <someshdiwan369@gmail.com>
2025-06-14 19:32:09 +0530

    feat: Add Sum class to calculate the sum of two floating-point numbers
    Add a program that:
    - Prompts the user to input two float values
    - Computes and prints their sum
    - Demonstrates use of `Scanner` for input and basic arithmetic with float types in Java.

--- Section5OperatorExpression/src/Sum.java
+++ Section5OperatorExpression/src/Sum.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter a two numbers: ");
+
         float num1 = input.nextFloat();
         float num2 = input.nextFloat();
-
         float sum = num1 + num2;
 
         System.out.println("Sum = " + sum);

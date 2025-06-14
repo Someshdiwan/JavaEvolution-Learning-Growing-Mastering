85752ae55647b6ceff899ce0a4368e02a41c27e8
Somesh diwan <someshdiwan369@gmail.com>
2025-06-14 19:14:04 +0530

    feat: Add BooleanCalculator class to perform integer multiplication
    Add a new class that demonstrates basic integer multiplication using variables.
    Includes:
    - Declaration and initialization of integer and boolean variables
    - Multiplication of three integers
    - Output of the result.

--- Section5OperatorExpression/1. Fundamentals/Operators/1.6 Boolean Calculator/src/BooleanCalculator.java
+++ Section5OperatorExpression/1. Fundamentals/Operators/1.6 Boolean Calculator/src/BooleanCalculator.java
@@ -2,7 +2,12 @@ public class BooleanCalculator
 {
    public static void main(String[] args)
    {
-      int a = 3, b = -4, c = 1;
+      int a = 3,
+              b = -4,
+              c = 1;
+
       boolean t = true, f = false;
+      int z = a*b*c;
+      System.out.println("Multiplication of the numbers is: "+z);
    }
 }

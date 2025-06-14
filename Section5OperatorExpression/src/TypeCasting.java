1ebf85e16904612c644fdda1f310d43ed28e0dbf
Somesh diwan <someshdiwan369@gmail.com>
2025-06-14 19:39:07 +0530

    feat: Add TypeCasting class demonstrating manual casting and type promotion
    Add a class that:
    - Demonstrates explicit type casting from float to int
    - Shows automatic type promotion in arithmetic expressions
    - Combines various primitive types in a mixed expression
    - Illustrates how Java handles data type conversion and expression evaluation.

--- Section5OperatorExpression/src/TypeCasting.java
+++ Section5OperatorExpression/src/TypeCasting.java
@@ -3,32 +3,34 @@ import java.util.Scanner;
 public class TypeCasting {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        float num = input.nextFloat();
-//        int num = input.nextInt();
-//        System.out.println(num);
+
+        /*float num = input.nextFloat();
+        int num = input.nextInt();
+        System.out.println(num);*/
 
         // type casting
         int num = (int)(67.56f);
-//        System.out.println(num);
+        System.out.println(num);
 
-        // automatic type promotion in expressions
-//        int a = 257;
-//        byte b = (byte)(a); // 257 % 256 = 1
+         /*automatic type promotion in expressions*/
 
-//        byte a = 40;
-//        byte b = 50;
-//        byte c = 100;
-//        int d = a * b / c;
-//
-//        System.out.println(d);
+        /*int a = 257;
+        byte b = (byte)(a); // 257 % 256 = 1
 
-//        byte b = 50;
-//        b = b * 2;
+        byte a = 40;
+        byte b = 50;
+        byte c = 100;
+        int d = a * b / c;
 
-//        int number = 'A';
-//        System.out.println("你好");
+        System.out.println(d);
 
-//        System.out.println(3 * 6);
+        byte b = 50;
+        b = b * 2;
+
+        int number = 'A';
+        System.out.println("你好");
+
+        System.out.println(3 * 6);*/
 
         byte b = 42;
         char c = 'a';
@@ -36,7 +38,9 @@ public class TypeCasting {
         int i = 50000;
         float f = 5.67f;
         double d = 0.1234;
+
         double result = (f * b) + (i / c) - (d * s);
+
         // float + int - double = double
         System.out.println((f * b) + " " + (i / c) + " " + (d * s));
         System.out.println(result);

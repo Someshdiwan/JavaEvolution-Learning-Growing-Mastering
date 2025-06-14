d29158059b3fdd50fbf93e17d276b1cec5e57b40
Somesh diwan <someshdiwan369@gmail.com>
2025-06-14 19:20:27 +0530

    feat: Add Bitwise1 class demonstrating bitwise AND operation
    Add a program that:
    - Initializes two integers using binary literals
    - Performs a bitwise AND (`&`) operation
    - Prints the result of the bitwise computation
    - Demonstrates basic use of bitwise operators in Java.

--- Section5OperatorExpression/src/Bitwise1.java
+++ Section5OperatorExpression/src/Bitwise1.java
@@ -1,16 +1,11 @@
-public class Bitwise1
-{
-    public static void main(String[] args)
-    {
+public class Bitwise1 {
+    public static void main(String[] args) {
         int x = 0b1010; //10;
-
         int y= 0b0110; //6;
 
         int z;
 
-        z = x & y;
-
-        //And Operation
+        z = x & y; //And Operation
 
         System.out.println(z);
     }

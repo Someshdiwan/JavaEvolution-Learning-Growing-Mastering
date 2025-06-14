1929652f2c64c306b5124367e22cdf1b55fb4866
Somesh diwan <someshdiwan369@gmail.com>
2025-06-14 19:28:24 +0530

    feat: Add Inputs class to demonstrate reading different data types from user
    Add a program that:
    - Takes roll number (int), name (String), and marks (float) as input
    - Uses `Scanner` to read and store user input
    - Prints the collected data to the console
    - Demonstrates handling of multiple primitive data types via standard input.

--- Section5OperatorExpression/src/Inputs.java
+++ Section5OperatorExpression/src/Inputs.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        System.out.print("Please enter some input: ");
-//        int rollno = input.nextInt();
-//        System.out.println("Your roll number is " + rollno);
 
-//        String name = input.next();
-//        System.out.println(name);
+        System.out.print("Please enter your Roll No: ");
+        int rollno = input.nextInt();
+        System.out.println("Your Roll No is: " + rollno);
 
-//        float marks = input.nextFloat();
-//        System.out.println(marks);
+        System.out.println("Please enter your Name: ");
+        String name = input.next();
+        System.out.println(name);
+
+        System.out.println("Please enter your Marks: ");
+        float marks = input.nextFloat();
+        System.out.println(marks);
     }
 }

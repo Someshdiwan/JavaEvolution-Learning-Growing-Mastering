1364e2bc73f176504971b8c754c2165a5b802296
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 15:57:48 +0530

    feat: Count total number of digits in an integer
    🧠 Logic:
    - Given an integer `n = 467777777`, we divide it repeatedly by 10 to remove its last digit.
    - Each division operation indicates one digit processed, so we increment a `res` counter.
    - The loop continues until `n` becomes 0.
    - Finally, `res` holds the total number of digits in the original number (here, 9).
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section7ConditionalStatements/src/CountsDigits.java
+++ Section7ConditionalStatements/src/CountsDigits.java
@@ -1,10 +1,11 @@
-public class countsDigits {
+public class CountsDigits {
     public static void main(String[] args) {
         int n=467777777;
+
         System.out.println(n/10);
+
         int res = 0;
-        while (n>0)
-        {
+        while (n>0) {
             n = n/10;
             res++;
         }

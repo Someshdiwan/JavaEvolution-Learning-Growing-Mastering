93cecb2229fbe30bbe993fa695d3875b97bbd161
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 21:37:21 +0530

    feat: Generate arithmetic progression series up to n terms
    🧠 Logic:
    - Read first term `a`, common difference `d`, and number of terms `n`.
    - Initialize `term` with `a`.
    - Use a loop running `n` times:
      - Print current `term`
      - Update `term = term + d` to get the next AP value.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/ArithimaticSeries.java
+++ Section8LoopAB/src/ArithimaticSeries.java
@@ -9,17 +9,13 @@ public class ArithimaticSeries {
         System.out.println("Enter a, d and n");
 
         int a = sc.nextInt();
-
         int d= sc.nextInt();
-
         int n = sc.nextInt();
 
         int term = a;
 
-        for(int i=0; i<n; i++)
-        {
+        for(int i=0; i<n; i++) {
             System.out.print(term+",");
-
             term=term+d;
         }
     }

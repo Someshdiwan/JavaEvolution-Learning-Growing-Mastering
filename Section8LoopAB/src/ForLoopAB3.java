24f68882806580ddea62bd7b305935d651adda05
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 01:44:56 +0530

    feat: Display multiplication table of a user-entered number using for-loop
    🧠 Logic:
    - Take integer input `n` from the user.
    - Use a `for` loop from 0 to 10 to print the multiplication table.
    - On each iteration, print the formatted result: `n x i = product`.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/ForLoopAB3.java
+++ Section8LoopAB/src/ForLoopAB3.java
@@ -3,13 +3,12 @@
 import java.util.Scanner;
 
 public class ForLoopAB3 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number");
-        int n = sc.nextInt();
 
-        for(int i = 0; i<=10; i++){
+        int n = sc.nextInt();
+        for(int i = 0; i<=10; i++) {
             System.out.println(n+" x "+i+" = "+ n*i);
         }
     }

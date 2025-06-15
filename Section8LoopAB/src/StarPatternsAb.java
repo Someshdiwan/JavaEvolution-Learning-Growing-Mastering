78db5be3032c23d84c575890df53a8272e4fc4c1
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 02:21:11 +0530

    feat: Add right-aligned decreasing star pattern using nested loops
    🧠 Logic:
    - Outer loop controls rows (1 to 5).
    - Inner loop prints either:
       - "* " if current column (j) ≥ current row (i)
       - " " (space) otherwise
    - This creates a right-aligned decreasing triangle of stars.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/StarPatternsAb.java
+++ Section8LoopAB/src/StarPatternsAb.java
@@ -1,9 +1,7 @@
 public class StarPatternsAb {
     public static void main(String[] args) {
-        for (int i =1; i<=5; i++)
-        {
-            for (int j=1; j<=5; j++)
-            {
+        for (int i =1; i<=5; i++) {
+            for (int j=1; j<=5; j++) {
                 if(i<=j)
                     System.out.print("* ");
                 else

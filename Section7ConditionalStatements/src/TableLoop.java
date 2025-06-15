f79adb235d427016516f777449f785b77bbcc156
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 16:16:39 +0530

    feat: Print multiplication table of a number up to user-defined limit using while loop
    🧠 Logic:
    - Takes two inputs from the user: the number to print the table for and the limit up to which it should print.
    - Uses a `while` loop starting from 1 up to `limit`.
    - Prints the format: number x i = product on each iteration.
    - Ensures scanner is closed to free resources.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section7ConditionalStatements/src/TableLoop.java
+++ Section7ConditionalStatements/src/TableLoop.java
@@ -1,27 +1,27 @@
 //Program to Print the Table of a Given Number Using while Loop
 
 
-//import java.util.Scanner;
-//
-//public class TableLoop {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter a number: ");
-//        int number = sc.nextInt();
-//        int i = 1;  // Counter for the table
-//
-//        while (i <= 10) {
-//            System.out.println(number + " x " + i + " = " + (number * i));
-//            i++;
-//        }
-//    }
-//}
+/*import java.util.Scanner;
+
+public class TableLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int i = 1;  // Counter for the table
+
+        while (i <= 10) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+            i++;
+        }
+    }
+}*/
 
 import java.util.Scanner;
 
 public class TableLoop {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // Create scanner object to take user input
+        Scanner sc = new Scanner(System.in); // Create a scanner object to take user input
 
         // Ask for the number to print its multiplication table
 
@@ -42,6 +42,6 @@ public class TableLoop {
             System.out.println(number + " x " + i + " = " + (number * i)); // Print the multiplication
             i++; // Increment the counter
         }
-        sc.close(); // Close the scanner to free resources
+        sc.close();
     }
 }

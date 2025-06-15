ec43a172b530b877be5c7a79f03a67f19abc3821
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 15:58:15 +0530

    feat: Calculate factorial of a number using while loop
    🧠 Logic:
    - Start with `n = 6` and initialize `result = 1`.
    - Multiply `result` by `n` in each iteration and decrement `n` by 1.
    - Continue until `n` reaches 0.
    - This computes `6! = 6×5×4×3×2×1 = 720` and prints the result.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section7ConditionalStatements/src/Factorial.java
+++ Section7ConditionalStatements/src/Factorial.java
@@ -2,9 +2,9 @@ public class Factorial {
     public static void main(String[] args) {
         int n =6;
         int result = 1;
-        while(n>0)
-        {
-            result = result *n;
+
+        while(n>0) {
+            result = result * n;
             n--;
         }
         System.out.println(result);

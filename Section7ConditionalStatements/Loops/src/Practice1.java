2ed48ccb3b0361c219dad40e26a56950bb9a4fe8
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 12:44:12 +0530

    feat: Add reward eligibility logic based on points and VIP status
    ✅ Logic:
    - Checks multiple conditions using `if`, `||`, and `&&` operators
    - Determines reward eligibility like paper clips, notebook, or cutlery set
    - Prioritizes VIP clients or high-point thresholds.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section7ConditionalStatements/Loops/src/Practice1.java
+++ Section7ConditionalStatements/Loops/src/Practice1.java
@@ -1,17 +1,19 @@
-public class Practice
-{
-    public static void main(String[] args)
-    {
-        int points = /* Replace with an appropriate value */;
-        boolean isVIPClient = /* Replace with an appropriate value */;
+public class Practice1 {
+    public static void main(String[] args) {
+        int points = 100;
+        boolean isVIPClient = true;
 
-        if (points >= 10 || isVIPClient)
+        if (points >= 10 || isVIPClient) {
             System.out.println("Dear customer, your purchase has accredited you with points.");
-        if (points > 10)
+        }
+        if (points > 10) {
             System.out.println("You may spend your points on a set of " + points * 5 + " paper clips.");
-        if (points >= 99 && isVIPClient)
+        }
+        if (points >= 99 && isVIPClient) {
             System.out.println("You may spend your points on a notebook.");
-        if (points > 149 || isVIPClient)
+        }
+        if (points > 149 || isVIPClient) {
             System.out.println("You may spend your points on a cutlery set.");
+        }
     }
 }

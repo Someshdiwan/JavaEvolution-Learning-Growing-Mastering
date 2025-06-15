d20f278c94e0e1f45e505196eb859bbec44f8061
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 02:22:13 +0530

    feat: Add left-aligned decreasing star pattern using nested loops
    🧠 Logic:
    - Outer loop runs from 1 to 5 for each row.
    - Inner loop runs from 1 to (5 - i + 1) to print decreasing stars.
    - Pattern displays a left-aligned triangle with decreasing stars per row.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/StarPatternsAB1.java
+++ Section8LoopAB/src/StarPatternsAB1.java
@@ -1,9 +1,7 @@
 public class StarPatternsAB1 {
     public static void main(String[] args) {
-        for (int i =1; i<=5; i++)
-        {
-            for (int j=1; j<=5-i+1; j++)
-            {
+        for (int i =1; i<=5; i++) {
+            for (int j=1; j<=5-i+1; j++) {
                 System.out.print("* ");
             }
             System.out.println("");

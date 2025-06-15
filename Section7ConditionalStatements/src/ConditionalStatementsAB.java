e5840abae9893e779bd12cfd20a50fddb689123a
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 14:41:48 +0530

    feat: Add demo of basic conditional statements using relational and logical operators
    🧠 Logic:
    - Compares `a`, `b`, and `c` using relational operators (`<`, `>`) and prints results.
    - Evaluates combined conditions using logical AND (`&&`) and OR (`||`) to show how compound expressions behave.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section7ConditionalStatements/src/ConditionalStatementsAB.java
+++ Section7ConditionalStatements/src/ConditionalStatementsAB.java
@@ -3,7 +3,6 @@
 public class ConditionalStatementsAB {
     public static void main(String[] args) {
         int a = 5, b=10, c=15;
-
         System.out.println(a<b);
         System.out.println(a<b && a<c);
         System.out.println(a<b || a>c);

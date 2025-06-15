85350126c6161c4b94e865449b3dec59a083f445
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 21:22:51 +0530

    feat: Print right-angled triangle star pattern using nested loops
    🧠 Logic:
    - Take input `n` from user to define the number of rows.
    - Outer loop (`i`) runs from 1 to `n` to manage rows.
    - Inner loop (`j`) runs from 1 to `i` to print increasing stars on each row.
    - Print a newline after each row to form the triangle shape.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/Loop 2.O/src/NestedLoopCC.java
+++ Section8LoopAB/Loop 2.O/src/NestedLoopCC.java
@@ -1,21 +1,17 @@
-//Right angle triangle pattern
-
 import java.util.Scanner;
 
 public class NestedLoopCC {
     public static void main(String[] args) {
-        //take input from user and print
+        //Right angle triangle pattern.
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
         int n = scanner.nextInt();
-
-        for(int i = 1; i <= n; i++)
-        {
-            for(int j = 1; j<= i; j++)
-            {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j<= i; j++) {
                 System.out.print("* ");
             }
-            System.out.println(); //New line pe hoga print star
+            System.out.println(); //New line pe Hoga print star
         }
     }
 }

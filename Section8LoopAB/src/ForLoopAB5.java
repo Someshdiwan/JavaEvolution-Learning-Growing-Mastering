fded6ba907e5d11c8f022d3524118625aaea617d
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 01:48:08 +0530

    feat: Calculate sum of first N natural numbers using for loop
    🧠 Logic:
    - Read user input `n` for number of terms.
    - Initialize sum to 0.
    - Loop from 1 to `n`, adding each number to `sum`.
    - Print the final sum after the loop completes.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/ForLoopAB5.java
+++ Section8LoopAB/src/ForLoopAB5.java
@@ -1,22 +1,3 @@
-/*
-Find the sum of First 10 Natural Numbers.
-
-n=10
-
-0+1=1
-1 1
-2 1+2=3
-3 3+3=6
-6 6+4=10
-10 10+5=15
-15 15+6=21
-
-Sum = 0; initially
-Sum = Sum + i value
-
-logic for(int i=1; i<=n; i++){}
-*/
-
 import java.util.Scanner;
 
 public class ForLoopAB5 {
@@ -26,10 +7,33 @@ public class ForLoopAB5 {
         int n = sc.nextInt();
 
         int sum=0;
-        for(int i=1; i<=n; i++)
-        {
+        for(int i=1; i<=n; i++) {
             sum=sum+i;
         }
         System.out.println("Sum of "+n+" Number is "+sum);
     }
 }
+
+/*
+Find the sum of the First 10 Natural Numbers.
+
+n=10
+
+0+1 = 1
+
+1 1
+2 1+2=3
+3 3+3=6
+6 6+4=10
+10 10+5 = 15
+15 15+6=21
+
+Sum = 0; initially
+Sum = Sum + i value
+
+logic:
+
+for(int i=1; i<=n; i++) {
+
+}
+*/

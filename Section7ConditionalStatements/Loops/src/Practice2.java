ad60e191c8e376dab7000d4ef4e6c620175fd4ff
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 12:46:27 +0530

    feat: Add height check logic for roller coaster eligibility
    ✅ Logic:
    - Checks if user's height is at least 1.5 meters
    - Grants or denies access based on condition.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section7ConditionalStatements/Loops/src/Practice2.java
+++ Section7ConditionalStatements/Loops/src/Practice2.java
@@ -1,12 +1,12 @@
-public class Practice2
-{
-    public static void main(String[] args)
-    {
+public class Practice2 {
+    public static void main(String[] args) {
         double height = 1.5; //in meters
         System.out.println("To enter the roller coaster, you must be at least 1.5 meters tall.");
-        if (/* Replace with the appropriate condition */)
+
+        if (height >= 1.5) {
             System.out.println("You may proceed.");
-        else
+        } else {
             System.out.println("You may not ride it.");
+        }
     }
 }

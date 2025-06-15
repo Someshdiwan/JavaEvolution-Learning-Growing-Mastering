ec5d2ec731723642237b4ff5e3415f59b079bf22
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 01:57:48 +0530

    feat: Compute factorial of a number using a for loop
    🧠 Logic:
    - Read user input `n`.
    - Initialize `fact` as 1 (to hold the factorial result).
    - Loop from 1 to `n`, multiplying `fact` by the loop counter `i` at each step.
    - After the loop, print the final factorial value.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/ForLoopAB6.java
+++ Section8LoopAB/src/ForLoopAB6.java
@@ -7,10 +7,11 @@ public class ForLoopAB6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number: ");
+
         int n = sc.nextInt();
+
         long fact =1;
-        for(int i =1;  i<=n; i++)
-        {
+        for(int i =1;  i<=n; i++) {
             fact = fact * i;
         }
         System.out.println("Factorial of number is: "+fact);

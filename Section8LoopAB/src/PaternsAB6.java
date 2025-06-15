cf1a6ca8b91f0ce7ba905b11cdce442860e66298
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 02:06:04 +0530

    feat: Print right-angled triangle star pattern using nested loops
    🧠 Logic:
    - Outer loop controls the number of rows (1 to 5).
    - Inner loop prints increasing number of stars per row.
    - Each row ends with a newline to form a right-angled triangle.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/PaternsAB6.java
+++ Section8LoopAB/src/PaternsAB6.java
@@ -1,9 +1,7 @@
 public class PaternsAB6 {
     public static void main(String[] args) {
-        for (int i =1; i<=5; i++)
-        {
-            for (int j=1; j<=i; j++)
-            {
+        for (int i =1; i<=5; i++) {
+            for (int j=1; j<=i; j++) {
                 System.out.print("* ");
             }
             System.out.println("");

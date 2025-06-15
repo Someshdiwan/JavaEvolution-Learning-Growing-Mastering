f7360055e17fad6230a9c024fba22eb7f7bd3783
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 21:52:20 +0530

    feat: Generate Fibonacci series up to n terms using iterative loop
    🧠 Logic:
    - Read `n` from user input representing number of terms to generate.
    - Start with base values: a = 0, b = 1.
    - Print the first two terms (0, 1).
    - Loop from 0 to n-2 to generate remaining terms:
      - Compute next term `c = a + b`.
      - Print `c`, then update `a = b` and `b = c`.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/FibonacciSeries.java
+++ Section8LoopAB/src/FibonacciSeries.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class FibonacciSeries {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+
         System.out.println("Program to Fibonacci Series");
         System.out.println("Enter number to Terms");
 
@@ -12,8 +13,7 @@ public class FibonacciSeries {
 
         System.out.print(a+","+b+",");
 
-        for(int i =0; i<n-2;i++)
-        {
+        for(int i =0; i<n-2;i++) {
             c=a+b;
             System.out.print(c+",");
             a=b;

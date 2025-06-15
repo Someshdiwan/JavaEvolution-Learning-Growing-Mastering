1f56cfafd713c8badbefafce01ebd116868f7d2c
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 19:38:05 +0530

    feat: Accept array input from user and print even numbers
    🧠 Logic:
    - Read the array size `n` from the user and create an array `A` of size `n`.
    - Accept `n` elements from the user using a for loop.
    - Traverse the array and check each element:
        - If the element is divisible by 2 (`A[i] % 2 == 0`), print it as an even number.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/Loop 2.O/src/A2.java
+++ Section8LoopAB/Loop 2.O/src/A2.java
@@ -1,26 +1,22 @@
 import java.util.Scanner;
 
-class A2
-{
-    public static void main(String[] args)
-    {
+class A2 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
         System.out.println("Enter size of the Array");
         n = sc.nextInt();
+
         int [] A = new int[n];
         System.out.println("Enter n elements of");
 
-        for(int i =0; i<A.length; i++)
-        {
+        for(int i =0; i<A.length; i++) {
             A[i] = sc.nextInt();
         }
-
         System.out.println("Even Numbers are: ");
-        for(int i = 0; i<A.length; i++)
-        {
-            if(A[i]%2==0)
-            {
+
+        for(int i = 0; i<A.length; i++) {
+            if(A[i]%2==0) {
                 System.out.println(A[i]);
             }
         }

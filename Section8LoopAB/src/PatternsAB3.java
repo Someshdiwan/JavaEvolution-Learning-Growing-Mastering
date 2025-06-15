b9df52a2234c3f49e5d7c569251a51f055da03c7
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 02:12:20 +0530

    feat: Generate grid pattern with i+j sum using nested loops
    🧠 Logic:
    - Outer loop runs from 1 to 5 for each row.
    - Inner loop calculates and prints the sum of current row index `i` and column index `j`.
    - Produces a pattern where each element increases diagonally from left to right.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/PatternsAB3.java
+++ Section8LoopAB/src/PatternsAB3.java
@@ -1,11 +1,8 @@
 public class PatternsAB3 {
     public static void main(String[] args) {
         int count =0;
-
-        for (int i =1; i<=5; i++)
-        {
-            for (int j=1; j<=5; j++)
-            {
+        for (int i =1; i<=5; i++) {
+            for (int j=1; j<=5; j++) {
                 count++;
                 System.out.format("%02d ",count);  //IMP
             }

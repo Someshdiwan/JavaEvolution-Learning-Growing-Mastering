221496463a8026a668f6da40327742461a3cd877
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 12:39:21 +0530

    feat: Print all odd numbers from 1 to 100 using for loop
    🧮 Logic:
    - Loop from 1 to 100
    - Use condition `value % 2 != 0` to filter and print odd numbers only.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section7ConditionalStatements/Loops/src/Loops.java
+++ Section7ConditionalStatements/Loops/src/Loops.java
@@ -1,13 +1,13 @@
-public class Loops
-{
-    public static void main(String[] args)
-    {
+public class Loops {
+    public static void main(String[] args) {
         System.out.println("These are the odd numbers from 1 to 100.");
         System.out.println("Ready?");
+
         //for values from 1 to 100
-        for (int value = 1; value <= 100; value++)
-        {
-            System.out.println(value); //print them
+        for (int value = 1; value <= 100; value++) {
+            if (value % 2 != 0) {
+                System.out.println(value);
+            }
         }
         System.out.println("That was fast!");
     }

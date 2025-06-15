4d49764842d57a7337b2ee140b4494d1fb5585bb
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 11:48:58 +0530

    feat: Mirror a string by concatenating it with itself for multiple test cases
    🧠 Logic:
    - Read the number of test cases `t`.
    - For each test case:
      - Read a string `s`.
      - Create a new string `x` by concatenating `s + s`.
      - Print `x`.
    
    📌 Purpose:
    Demonstrates string manipulation using concatenation in Java and looping through multiple inputs efficiently.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section6StringClassPrinting/src/StringConcationation.java
+++ Section6StringClassPrinting/src/StringConcationation.java
@@ -1,7 +1,5 @@
-public class StringConcationation
-{
-    public static void main(String[] args)
-    {
+public class StringConcationation {
+    public static void main(String[] args) {
         String s = "5";
         System.out.println(s + 5);
     }

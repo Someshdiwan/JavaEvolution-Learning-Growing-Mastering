28b493f84ee0093f64a1db039eaab0a49c810726
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 14:41:21 +0530

    feat: Demonstrate use of break in while loop to stop iteration at specific condition
    🧠 Logic:
    - Starts printing numbers from 0 using a `while` loop.
    - When the counter `i` reaches 500, the `break` statement terminates the loop immediately.
    - Prints "Break and Continue" after exiting the loop.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section7ConditionalStatements/src/Break.java
+++ Section7ConditionalStatements/src/Break.java
@@ -1,9 +1,11 @@
 public class Break {
     public static void main(String[] args) {
         int i =0;
-        while(i<10000){
+
+        while(i<10000) {
             System.out.println(i);
-            if(i==500){
+
+            if(i==500) {
                 break;
             }
             i++;

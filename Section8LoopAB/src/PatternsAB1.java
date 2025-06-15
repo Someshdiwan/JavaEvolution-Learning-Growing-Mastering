7ae3f33207dd5a7e3f0d18b049ffd9fcece09499
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 02:09:11 +0530

    feat: Print square pattern with repeating row numbers using nested loops
    🧠 Logic:
    - Outer loop controls the rows (1 to 5).
    - Inner loop prints the current row number 5 times.
    - Results in a square grid with identical numbers in each row.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/PatternsAB1.java
+++ Section8LoopAB/src/PatternsAB1.java
@@ -1,10 +1,8 @@
 public class PatternsAB1 {
     public static void main(String[] args) {
-        for (int i =1; i<=5; i++)
-        {
-            for (int j=1; j<=5; j++)
-            {
-                System.out.print(i+" "); //Observation is important to draw a patterns.
+        for (int i =1; i<=5; i++) {
+            for (int j=1; j<=5; j++) {
+                System.out.print(i+" "); //Observation is important to draw a pattern.
             }
             System.out.println(" ");
         }

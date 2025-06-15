0165a4203cb1d634be5b98ec57ba77cdedfed45f
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 20:27:48 +0530

    feat: Search for an element in an array using linear search
    🧠 Logic:
    - Takes the size of the array and its elements from the user.
    - Prints all array elements.
    - Asks the user for an element to search (`s`) in the array.
    - Uses a linear search (`for` loop) to check each element:
      - If match is found, sets `x = 1` and stores the index in `pos`, then breaks the loop.
    - After search:
      - If `x == 0`, element was not found.
      - If `x == 1`, displays the index where the element was found.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/Loop 2.O/src/LoopsPractice.java
+++ Section8LoopAB/Loop 2.O/src/LoopsPractice.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 class LoopsPractice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n, s, x = 0, pos = 0;
+
         System.out.println("Enter size of an array");
         n = sc.nextInt();
+
         int[] A = new int[n];
         System.out.println("Enter Array elements:");
 
         for (int i = 0; i < A.length; i++) {
             A[i] = sc.nextInt();
         }
-        System.out.println("Array elements are: ");
 
+        System.out.println("Array elements are: ");
         for (int i = 0; i < A.length; i++) {
             System.out.println(A[i]);
         }

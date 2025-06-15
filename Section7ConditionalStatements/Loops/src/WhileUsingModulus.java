e8c4066df95b301c75cd1f5de1e8879564530bec
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 12:57:26 +0530

    feat: Count numbers divisible by both 2 and 3 from 1 to 10 using while loop
    ✅ Logic:
    - Iterates from 1 to 10
    - Checks divisibility by both 2 and 3 using modulus
    - Increments counter if condition is true and prints results.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section7ConditionalStatements/Loops/src/WhileUsingModulus.java
+++ Section7ConditionalStatements/Loops/src/WhileUsingModulus.java
@@ -1,18 +1,22 @@
-public class WhileUsingModulus
-{
-    public static void main(String[] args)
-    {
+public class WhileUsingModulus {
+    public static void main(String[] args) {
         int num = 1;
         int count = 0;
-        //while number is less than, or equal to, 10
-        while (num <= 10)
-        {
-            //if the number is divisible by 2 (i.e. even) AND divisible by 3, then...
-            if (num % 2 == 0 && num % 3 == 0)
-            {
+
+        System.out.println("Checking numbers from 1 to 10:");
+
+        while (num <= 10) {
+            System.out.print("Number " + num + " is ");
+
+            if (num % 2 == 0 && num % 3 == 0) {
+                System.out.println("divisible by both 2 and 3!");
                 count++;
+            } else {
+                System.out.println("not divisible by both 2 and 3");
             }
             num++;
         }
+
+        System.out.println("\nTotal numbers divisible by both 2 and 3: " + count);
     }
 }

857ae5db9366a8fbed3152e4beb4752180ab9cf0
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 02:25:12 +0530

    feat: Create right-aligned lower triangle star pattern using i + j > 5 condition
    🧠 Logic:
    - Outer loop runs for rows (1 to 5).
    - Inner loop runs for columns (1 to 5).
    - If (i + j > 5), print a star (`* `); else, print double space.
    - This results in a right-aligned triangle formed by stars in the lower half.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/StarPatternsAB4.java
+++ Section8LoopAB/src/StarPatternsAB4.java
@@ -1,15 +1,14 @@
 public class StarPatternsAB4 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
+            for (int j = 1; j <= 5; j++)
+            {
                 if (i + j > 5)
                     System.out.print(("* "));
                 else
                     System.out.print("  ");
             }
-
             System.out.println("");
-
         }
     }
 }

20c76c97686a6a4303517ddcf44c18801498b1ee
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 02:04:37 +0530

    feat: Check if a number is a palindrome using reverse logic
    🧠 Logic:
    - Read a number from user input.
    - Reverse the number by extracting and rebuilding digits.
    - Compare the reversed number with the original.
    - If both are equal, it's a palindrome (reads the same forward and backward).
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/src/PalindromeANumber.java
+++ Section8LoopAB/src/PalindromeANumber.java
@@ -1,20 +1,18 @@
-//Palindrome number means, a reverse of a
-//number is a number then it is called as a palindrome number.
+//Palindrome number means, a reverse of a number is a number then it is called as a palindrome number.
 
 import java.util.Scanner;
-public class PalindromeANumber
-{
-    public static void main(String[] args)
-    {
+public class PalindromeANumber {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number: ");
 
         int n = sc.nextInt();
 
         int m = n; //Hold A Number into another Variable.
+
         int rev =0, r;
-        while (n>0)
-        {
+
+        while (n>0) {
             r=n%10;
             rev=rev*10+r;
             n/=10;

8253cd15f3fa17c4b4e086f29b6202af7150bc73
Somesh diwan <someshdiwan369@gmail.com>
2025-06-15 19:36:38 +0530

    feat: Print elements of an integer array using a for loop
    🧠 Logic:
    - Declared and initialized an integer array `A` with values {11, 22, 33, 44, 55}.
    - Used a standard `for` loop to iterate from index 0 to `A.length - 1`.
    - Printed each element during iteration using `System.out.println(A[i])`.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section8LoopAB/Loop 2.O/src/A1.java
+++ Section8LoopAB/Loop 2.O/src/A1.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
 
-class A1
-{
-    public static void main(String[] args)
-    {
+class A1 {
+    public static void main(String[] args) {
         int [] A = {11,22,33,44,55};
         
-        for(int i=0; i<A.length; i++)
-        {
+        for(int i=0; i<A.length; i++) {
             System.out.println(A[i]);
         }
     }

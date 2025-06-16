572037ba6efc4b68615a30faff67209ba6406335
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 11:06:00 +0530

    feat: Read and display a 2x2 matrix using nested loops
    🧠 Logic:
    - Declare a 2x2 integer matrix.
    - Use nested `for` loops to input values from the user.
    - Use nested loops again to print the matrix in row-major order.
    - Demonstrates basic 2D array handling with Scanner input.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section9ArrayAB/Array IMP/TwoDArrayPractice.java
+++ Section9ArrayAB/Array IMP/TwoDArrayPractice.java
@@ -1,29 +1,25 @@
 import java.util.Scanner;
 
-class A2D
-{
-    public static void main(String[] args)
-    {
+class TwoDArrayPractice {
+    public static void main(String[] args) {
         int [][] A = new int [2][2];
+        //Declaring a 2x2 2D Array.
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the elements: ");
-        for(int i = 0; i<2; i++)
-        {
-            for(int j = 0; j<2; j++)
-            {
-                A[i][j] = nextInt[];
+
+        for(int i = 0; i<2; i++) {
+            for(int j = 0; j<2; j++) {
+                A[i][j] = sc.nextInt();
             }
         } 
 
         System.out.println("Array elements are: ");
-        for(int i = 0; i<2; i++)
-        {
-            for (int j = 0; j<2; j++)
-            {
+        for(int i = 0; i<2; i++) {
+            for (int j = 0; j<2; j++) {
                 System.out.print(A[i][j]+" ");
             }
         }
-
         System.out.println("");
     }
 }

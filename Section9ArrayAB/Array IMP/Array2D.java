a9990cfdab05b85defc0430864189474b8bae045
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 20:48:51 +0530

    feat: Accept and print a 2x2 matrix using nested loops
    🧠 Logic:
    - Create a 2x2 integer matrix.
    - Accept 4 user inputs to fill the matrix using nested `for` loops.
    - Print each element twice (as per current logic) in a row-wise manner.
    - Each row ends with a newline for better formatting.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section9ArrayAB/Array IMP/Array2D.java
+++ Section9ArrayAB/Array IMP/Array2D.java
@@ -1,24 +1,20 @@
 import java.util.Scanner;
-class Array2D
-{
-    public static void main(String [] args)
-    {
+class Array2D {
+    public static void main(String [] args) {
         int [][] A=new int [2][2];
         Scanner sc = new Scanner(System.in);
         System.out.println("enter 4 elements");
-        for(int i=0; i<2; i++)
-        {
-            for(int j=0; j<2; j++)
-            {
+
+        for(int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
                 A[i][j] = sc.nextInt();
             }
         }
         System.out.println("Array elements are");
-        for(int i=0; i<2; i++)
-        {
-            for(int j=0; j<2; j++)
-            {
-                System.out.println(A[i][j] + " "+ A[i][j]);
+
+        for(int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                System.out.print(A[i][j] + " ");
             }
         System.out.println(" ");
         }

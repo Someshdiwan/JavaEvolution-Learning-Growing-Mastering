68b6ac3b29e009c495a964c726e7c1b9b7390581
Somesh diwan <someshdiwan369@gmail.com>
2025-06-16 22:36:14 +0530

    feat: Access and print a specific element from an integer array
    🧠 Logic:
    - Declares and initializes an integer array with 7 elements.
    - Prints the element at index 2 (which is the third element, value 3).
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section9ArrayAB/Array IMP/ArrayCC.java
+++ Section9ArrayAB/Array IMP/ArrayCC.java
@@ -1,7 +1,5 @@
-public class ArrayCC
-{
-    public static void main(String[] args)
-    {
+public class ArrayCC {
+    public static void main(String[] args) {
         int[] num = {1,2,3,4,5,6,7};
         System.out.println(num[2]);
     }

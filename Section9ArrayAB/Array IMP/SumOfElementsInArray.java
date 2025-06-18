82ad8cef3d4fffae2ff7d39d96df976b4fa9458e
Somesh diwan <someshdiwan369@gmail.com>
2025-06-19 02:21:15 +0530

    feat: Calculate sum of all elements in an array using enhanced for loop
    🧠 Logic:
    - Declare and initialize an integer array `A` with 10 elements.
    - Initialize a variable `sum` to 0.
    - Use an enhanced for loop to iterate over each element in the array and add it to `sum`.
    - Print the total sum after the loop completes.
    - (Optional) Includes a commented version using traditional for loop for alternative approach.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section9ArrayAB/Array IMP/SumOfElementsInArray.java
+++ Section9ArrayAB/Array IMP/SumOfElementsInArray.java
@@ -1,22 +1,18 @@
 //Find the sum of all elements in the array.
 
-public class SumOfElementsInArray
-{
-    public static void main(String[] args)
-    {
+public class SumOfElementsInArray {
+    public static void main(String[] args) {
         int A[]={3,9,7,8,12,6,15,5,4,10};
 
         int sum = 0;
 
-        for(int x:A)
-        {
+        for(int x:A) {
             sum=sum + x;
         }
         System.out.println("Sum of The Total Number is:  "+ sum);
 
         /*
-        for (int i=0; i<A.length; i++)
-        {
+        for (int i=0; i<A.length; i++){
             sum=sum+A[i];
         }
         System.out.println("Sum of total number is: "+sum);

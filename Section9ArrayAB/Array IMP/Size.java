4dddb42832565c4c3b174573cfbbd734b4cf8da2
Somesh diwan <someshdiwan369@gmail.com>
2025-06-18 23:46:51 +0530

    feat: Identify and print even numbers from a user-defined array
    🧠 Logic:
    - Prompt the user for array size and initialize the array accordingly.
    - Accept array elements using a loop.
    - Traverse the array and print elements that are divisible by 2 (even numbers).
    - Closes the Scanner after input to prevent resource leak.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section9ArrayAB/Array IMP/Size.java
+++ Section9ArrayAB/Array IMP/Size.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
-class Size
-{
-    public static void main (String []args)
-    {
+
+class Size {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
-        System.out.println("enter the size of array");
+
+        System.out.println("Enter the size of array");
         n = sc.nextInt();
-        int[]A = new int [n];
-        System.out.println("enter the size of array");
-        for (int i = 0; i <A.length; i++)
-        {
-            if (A[i]%2 == 0)
-            {
+
+        int[] A = new int[n];
+        System.out.println("Enter the elements of array");
+        // Read array elements
+        for (int i = 0; i < A.length; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        System.out.println("Even numbers in the array:");
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 == 0) {
                 System.out.println(A[i]);
             }
         }
+        sc.close(); // Close the scanner when done
     }
 }

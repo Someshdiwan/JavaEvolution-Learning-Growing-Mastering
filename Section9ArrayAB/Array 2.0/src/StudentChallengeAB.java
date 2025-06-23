0f451507b22df583dc713e6395f204f33048a06e
Somesh diwan <someshdiwan369@gmail.com>
2025-06-23 23:56:35 +0530

    feat: Extract and validate email components (username and domain)
    🧠 Logic:
    - Given a string in email format, extract username using `substring(0, indexOf('@'))`.
    - Extract domain using `substring(indexOf('@') + 1, end of string)`.
    - Check if domain starts with "Gmail" and extract actual domain name (before '.') to validate.
    - Used `equalsIgnoreCase("gmail")` for case-insensitive comparison.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section9ArrayAB/Array 2.0/src/StudentChallengeAB.java
+++ Section9ArrayAB/Array 2.0/src/StudentChallengeAB.java
@@ -1,22 +1,16 @@
 public class StudentChallengeAB {
     public static void main(String[] args) {
-
         String str = "Programmer@Gmail.com";
 
         int i = str.indexOf("@");
-
         String uname = str.substring(0 , i);
-
         String domainname = str.substring(i+1, str.length());
 
         System.out.println("UserName : " + uname);
-
         System.out.println("Domain is: " + domainname);
-
         System.out.println(domainname.startsWith("Gmail")); //Check Valid Gmail or not.
 
-        //Now we have to check whether this is domain name and gmail name is valid or not
-
+        //Now we have to check whether this is a domain name and gmail name is valid or not
         int j =domainname.indexOf(".");
         String name = domainname.substring(0, j);
         System.out.println(name.equalsIgnoreCase("gmail"));

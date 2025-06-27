80f94e26baa10515f0e321ac16efb112fbbf41e0
Somesh diwan <someshdiwan369@gmail.com>
2025-06-27 23:56:50 +0530

    feat: Demonstrate behavior of static and non-static variables across method calls
    🧠 Logic:
    - `static int a = 10;` is shared across all instances of the class.
    - `int b = 10;` is re-initialized every time `fun()` is called.
    - On each call to `fun()`:
      - `a` retains and increments its value.
      - `b` resets to 10, increments to 11, but the new value doesn't persist.
    
    Signed-off-by: Somesh diwan <someshdiwan369@gmail.com>

--- Section10Methods/LocalandGlobalVariables/src/staticvariable.java
+++ Section10Methods/LocalandGlobalVariables/src/staticvariable.java
@@ -1,13 +1,12 @@
 public class staticvariable {
     static int a=10;
-    void fun()
-    {
+
+    void fun() {
         int b=10;
         System.out.println(a+" "+b);
         ++a; ++b;
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         staticvariable s = new staticvariable();
         s.fun();
         s.fun();

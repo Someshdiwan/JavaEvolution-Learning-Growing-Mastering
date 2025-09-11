6aa3d7f35aab385febe80df9b4f27553f977209a
https://github.com/Someshdiwan <someshdiwan369@gmail.com>
2025-09-11 15:16:49 +0530

    Section 12 Inheritance
    Signed-off-by: https://github.com/Someshdiwan <someshdiwan369@gmail.com>

--- Section12Inheritance/src/ThisVSSuperIMP.java
+++ Section12Inheritance/src/ThisVSSuperIMP.java
@@ -1,57 +1,45 @@
-//Need Practice the same thing again.
-//Solve Again Revise This.
-
 class RectangleSuper{
     int length;
     int breadth;
 
     // Default constructor
-    RectangleSuper()
-    {
+    RectangleSuper() {
         length=breadth=1;
     }
 
     // Parameterized constructor
-    RectangleSuper(int l, int b)
-    {
+    RectangleSuper(int l, int b) {
         length=l;
         breadth=b;
     }
 }
 
-class CuboidSuper extends RectangleSuper
-{
+class CuboidSuper extends RectangleSuper {
     int height;
 
     // Default constructor with super()
-    CuboidSuper()
-    {
+    CuboidSuper() {
         super(); // Calls the default constructor of RectangleSuper
         height=1;
     }
 
-    CuboidSuper(int h)
-    {
+    CuboidSuper(int h) {
         super();
         height=h;
     }
 
-    CuboidSuper(int l, int b,int h)
-    {
+    CuboidSuper(int l, int b,int h) {
         super(l,b);
         height=h;
     }
 
-    int volume()
-    {
+    int volume() {
         return length*breadth*height;
     }
 }
 
-public class ThisVSSuperIMP
-{
-    public static void main(String[] args)
-    {
+public class ThisVSSuperIMP {
+    public static void main(String[] args) {
         CuboidSuper c= new CuboidSuper(5,3,7);
         System.out.println("Volumne of Cuboid: "+c.volume());
     }

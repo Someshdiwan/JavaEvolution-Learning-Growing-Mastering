95e135076f122b8462d75206948d6ad8ac208676
https://github.com/Someshdiwan <someshdiwan369@gmail.com>
2025-09-12 20:48:29 +0530

    Section 15 Inner Classes
    Signed-off-by: https://github.com/Someshdiwan <someshdiwan369@gmail.com>

--- Section15InnerClasses/src/StaticInnerClass.java
+++ Section15InnerClasses/src/StaticInnerClass.java
@@ -1,21 +1,16 @@
-class OuterStatic
-{
+class OuterStatic {
     int x=10;
     static int y=20;
 
-    static class MyStatic
-    {
-        public void show()
-        {
+    static class MyStatic {
+        public void show() {
             System.out.println(y);
         }
     }
 }
 
-public class StaticInnerClass
-{
-    public static void main(String[] args)
-    {
+public class StaticInnerClass {
+    public static void main(String[] args) {
         OuterStatic.MyStatic o = new OuterStatic.MyStatic();
         o.show();
     }

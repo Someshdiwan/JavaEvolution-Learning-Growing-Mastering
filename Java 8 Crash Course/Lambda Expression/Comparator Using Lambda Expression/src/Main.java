ede4d69a37c0868074b44df21627741a3a5d5c90
https://github.com/Someshdiwan <someshdiwan369@gmail.com>
2025-09-23 00:46:19 +0530

    feat: Comparator Using Lambda Expression
    Signed-off-by: https://github.com/Someshdiwan <someshdiwan369@gmail.com>

--- Java 8 Crash Course/Lambda Expression/Comparator Using Lambda Expression/src/Main.java
+++ Java 8 Crash Course/Lambda Expression/Comparator Using Lambda Expression/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This section is about the comparator using lambda expression.");
+        System.out.println("This section is all about the comparator using lambda expression.");
     }
 }

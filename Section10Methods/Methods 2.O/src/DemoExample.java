eab4d26002439e11996ab27d5a1e1e063ce5c039
https://github.com/Someshdiwan <someshdiwan369@gmail.com>
2025-09-23 01:47:42 +0530

    Section 10 Methods
    Signed-off-by: https://github.com/Someshdiwan <someshdiwan369@gmail.com>

--- Section10Methods/Methods 2.O/src/DemoExample.java
+++ Section10Methods/Methods 2.O/src/DemoExample.java
@@ -2,7 +2,7 @@ public class DemoExample {
     public static void main(String[] args) {
         String chacha = "Iron Man";
 
-        //outside main class declare a method 'greet' and print name.
+        //outside the main class declare a method 'greet' and print name.
         greet(chacha);
     }
     static void greet(String naam) {

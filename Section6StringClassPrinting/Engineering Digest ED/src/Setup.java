6963848b48a325ea05ad9fe6cf864a4c452e7fe7
https://github.com/Someshdiwan <someshdiwan369@gmail.com>
2025-09-24 03:17:02 +0530

    Section 6 String Class Printing
    Signed-off-by: https://github.com/Someshdiwan <someshdiwan369@gmail.com>

--- Section6StringClassPrinting/Engineering Digest ED/src/Setup.java
+++ Section6StringClassPrinting/Engineering Digest ED/src/Setup.java
@@ -1,5 +1,5 @@
 public class Setup {
     public static void main(String[] args) {
-        System.out.println("Setup is done on Mac.");
+        System.out.println("String Class Printing.");
     }
 }

1f21eb0a5a9a917f1f9459619ba61f3c92e24f28
https://github.com/Someshdiwan <someshdiwan369@gmail.com>
2025-09-25 09:28:15 +0530

    Map
    Signed-off-by: https://github.com/Someshdiwan <someshdiwan369@gmail.com>

--- Section 25 Collections Frameworks/Map Interface/src/Map.java
+++ Section 25 Collections Frameworks/Map Interface/src/Map.java
@@ -1,5 +1,5 @@
 public class Map {
     public static void main(String[] args) {
-        System.out.println("Map Separate hierarchy — does NOT extend Collection)");
+        System.out.println("Map Separate hierarchy, does NOT extend Collection)");
     }
 }
